c9891c60a94cb3be1b813b7979f80d7edde2ecf6
aminaali7 <aminaaligombe@gmail.com>
2025-07-01 11:46:03 +0100

    Update 5index.js

--- public/5index.js
+++ public/5index.js
@@ -4,26 +4,25 @@ const itemsDiv = document.getElementById("items")
 const input = document.getElementById("itemInput")
 const storageKey = "items";
 
-
 function renderItems() {
-    itemsDiv.innerHMTL = null;
+    itemsDiv.innerHTML = null;
 
     for (const [idx, item] of Object.entries(items)) {
         const container = document.createElement("div")
         container.style.marginBottom = "10px"
-
-        const text = document.createElemetnt("p")
+        
+        const text = document.createElement("p")
         text.style.display = "inline"
         text.style.marginRight = "10px"
-        text.textcontent = item;
+        text.textContent = item;
 
         const button = document.createElement("button")
         button.textContent = "Delete"
         button.onclick = () => removeItem(idx)
 
         container.appendChild(text)
-        container.appenChild(button)
-
+        container.appendChild(button)
+        
         itemsDiv.appendChild(container)
     }
 }
